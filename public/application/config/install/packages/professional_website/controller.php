97ee399851d395ef7778e431c791424cf8f9d6f1
Fabian Bitter <fabian@bitter.de>
2021-06-20 18:26:41 +0200

    Update

--- public/application/config/install/packages/professional_website/controller.php
+++ public/application/config/install/packages/professional_website/controller.php
@@ -2,11 +2,11 @@
 
 namespace Application\StartingPointPackage\ProfessionalWebsite;
 
-use Concrete\Core\Application\Application;
-use Concrete\Core\Backup\ContentImporter;
-use Concrete\Core\Package\Routine\AttachModeInstallRoutine;
-use Concrete\Core\Package\StartingPointInstallRoutine;
+use Concrete\Core\Package\Package;
+use Concrete\Core\Package\PackageService;
 use Concrete\Core\Package\StartingPointPackage;
+use Concrete\Core\Support\Facade\Application;
+use Concrete\Core\Validation\CSRF\Token;
 
 class Controller extends StartingPointPackage
 {
@@ -20,6 +20,32 @@ class Controller extends StartingPointPackage
     public function getPackageDescription()
     {
         return t("Starting point package to install a professional website.");
+    }
+
+    public function install_content()
+    {
+        parent::install_content();
+
+        /*
+         * CIF file format doesn't allow to perform a full content swap.
+         * So we need to install this package manually.
+         */
+
+        $app = Application::getFacadeApplication();
+        /** @var PackageService $packageService */
+        $packageService = $app->make(PackageService::class);
+        /** @var Token $token */
+        $token = $app->make(Token::class);
+
+        /** @noinspection PhpDeprecationInspection */
+        $pkgClass = Package::getClass("bitter_theme");
+
+        if ($pkgClass) {
+            $packageService->install($pkgClass, [
+                "pkgDoFullContentSwap" => 1,
+                "ccm_token" => $token->generate('install_options_selected')
+            ]);
+        }
     }
 
 }
