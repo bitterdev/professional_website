7198a4688605f7fa0a23c26b47811fef31a2bf42
Fabian Bitter <fabianbitter@MacBook-Air-von-Fabian-2.local>
2023-08-22 21:54:35 +0200

    Update

--- public/application/config/install/packages/professional_website/controller.php
+++ public/application/config/install/packages/professional_website/controller.php
@@ -2,50 +2,20 @@
 
 namespace Application\StartingPointPackage\ProfessionalWebsite;
 
-use Concrete\Core\Package\Package;
-use Concrete\Core\Package\PackageService;
 use Concrete\Core\Package\StartingPointPackage;
-use Concrete\Core\Support\Facade\Application;
-use Concrete\Core\Validation\CSRF\Token;
 
 class Controller extends StartingPointPackage
 {
     protected $pkgHandle = 'professional_website';
 
-    public function getPackageName()
+    public function getPackageName(): string
     {
         return t("Professional Website");
     }
 
-    public function getPackageDescription()
+    public function getPackageDescription(): string
     {
         return t("Starting point package to install a professional website.");
-    }
-
-    public function install_content()
-    {
-        parent::install_content();
-
-        /*
-         * CIF file format doesn't allow to perform a full content swap.
-         * So we need to install this package manually.
-         */
-
-        $app = Application::getFacadeApplication();
-        /** @var PackageService $packageService */
-        $packageService = $app->make(PackageService::class);
-        /** @var Token $token */
-        $token = $app->make(Token::class);
-
-        /** @noinspection PhpDeprecationInspection */
-        $pkgClass = Package::getClass("bitter_theme");
-
-        if ($pkgClass) {
-            $packageService->install($pkgClass, [
-                "pkgDoFullContentSwap" => 1,
-                "ccm_token" => $token->generate('install_options_selected')
-            ]);
-        }
     }
 
 }
